e9ce29537e49e157b0af7384a03356025cf1a0a2
Indrek Lasn <lasnindrek@gmail.com>
2019-12-12 16:25:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { client, q } from './config/db'
 function App() {
+
+  useEffect(() => {
+    client.query(
+      q.Paginate(
+        q.Match(
+          q.Ref('indexes/notes')))
+    )
+      .then((response) => {
+        const productRefs = response.data
+        // create new query out of todo refs. 
+        // https://docs.fauna.com/fauna/current/api/fql/
+        const getAllProductDataQuery = productRefs.map((ref) => {
+          return q.Get(ref)
+        })
+        // query the refs
+        return client.query(getAllProductDataQuery).then((data) => data)
+      })
+      .catch((error) => console.log('error', error.message))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,6 @@
 import faunadb from 'faunadb'
 
-
-const client = new faunadb.Client({ secret: process.env.FAUNADB_KEY })
+const client = new faunadb.Client({ secret: process.env.REACT_APP_FAUNADB_KEY })
 
 const q = faunadb.query
 
