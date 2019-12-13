07148ccee76b2a00b3bd11f565a4b835f4f5f7c2
Indrek Lasn <lasnindrek@gmail.com>
2019-12-13 15:56:06 +0200

    fetch notes and render

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { getAllNotes, createNote, deleteNote, editNote } from './api'
-function App() {
+import { Icon, Card, Avatar } from 'antd';
 
+
+function App() {
+  const [notes, setNotes] = useState([])
   useEffect(() => {
-    getAllNotes.then(res => console.log(res))
+    getAllNotes.then(res => setNotes(res))
     // createNote('clean kitchen').then(res=> console.log(res))
     // deleteNote("251637892935320064").then(res => console.log(res))
-    editNote('251638296424219147', 'new text').then(() => 'note updated successfully!')
-  }, [])
+    // editNote('251638296424219147', 'new text').then(() => 'note updated successfully!')
+  }, [notes])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-c">
+          {notes.map(note => (
+            <div key={note.ref.id} className="note-row" onClick={() => console.log('clicking on note')}>
+              <p style={{minWidth: '200px'}}>{note.data.text}</p>
+              <Icon theme="twoTone" style={{ fontSize: '16px', margin: '0 10px 0 0'}}  type="edit" />
+              <Icon theme="twoTone" style={{ fontSize: '16px'}} type="delete" />
+
+            </div>
+            ))
+          }
       </header>
     </div>
   );
--- src/config/db.js
+++ src/config/db.js
@@ -3,16 +3,16 @@ import faunadb from 'faunadb'
 const client = new faunadb.Client({ secret: process.env.REACT_APP_FAUNADB_KEY })
 const q = faunadb.query
 
-client.query(q.CreateCollection({ name: 'notes' }))
-.then((ret) => console.log(ret))
+// client.query(q.CreateCollection({ name: 'notes' }))
+// .then((ret) => console.log(ret))
 
-client.query(
-  q.CreateIndex({
-    name: 'notes',
-    source: q.Collection('notes'),
-  })
-)
-.then((ret) => console.log(ret))
+// client.query(
+//   q.CreateIndex({
+//     name: 'notes',
+//     source: q.Collection('notes'),
+//   })
+// )
+// .then((ret) => console.log(ret))
 
 
 export { client , q }
