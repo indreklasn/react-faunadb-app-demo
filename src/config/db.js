6f1453d13b00d9d64dde8102290be297872357c8
Indrek Lasn <lasnindrek@gmail.com>
2019-12-15 22:37:56 +0200

    remove console logs

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,6 @@ import faunadb from 'faunadb'
 const client = new faunadb.Client({ secret: process.env.REACT_APP_FAUNADB_KEY })
 const q = faunadb.query
 
-console.log(process.env.REACT_APP_FAUNADB_KEY)
 
 // client.query(q.CreateCollection({ name: 'notes' }))
 // .then((ret) => console.log(ret))
